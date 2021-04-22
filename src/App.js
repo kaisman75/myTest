13edcd69821b167cfc34bee199fd1c02619dcb40
kaisman75 <kaismanai1975gmail.com>
2021-04-22 23:37:17 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 function App() {
   return (
     <div className="App">
-      <h1>hello World</h1>
+      <div className="container">
+      <h1>To Do App</h1>
+      
+    </div>
+    <input className="form-control form-control-lg" type="text" placeholder="New Task"></input>
     </div>
   );
 }
